57358c2c9c6c3d4a05f79578e23e40d8cd58be62
WeiYi.Li <weiyi.just2@gmail.com>
2015-04-12 12:08:35 +0800

    保留Fragment实例，使得设备旋转时视频仍可继续播放。 (1)调用setRetainInstance(true)方法可保留fragment。已保留的fragment不会随activity一起被销毁。相反，它会被一直保留并在需要时原封不动的传递给新的activity。 (2)对于已保留的fragment实例，其全部实例变量值也将保持不变。 (3)销毁和重建fragment的视图，但无需销毁fragment自身（新的配置可能需要新的资源来匹配；当有更合适的匹配资源可以利用时，则需重新创建视图）。 (4)覆盖Fragment.onSaveInstanceState(...)方法与保留fragment方法的主要区别在于，数据可以保存多久。如只需短暂保留数据，能应对设备配置改变就可以了，则保留fragment可以很轻松地解决问题。如果是保存对象，则更能体会使用保留fragment的便利。因为我们再也无需操心要保存的对象是否已实现Serializable接口了。

--- src/me/li2/android/hellomoon/HelloMoonFragment.java
+++ src/me/li2/android/hellomoon/HelloMoonFragment.java
@@ -14,6 +14,12 @@ public class HelloMoonFragment extends Fragment {
     private Button mStopButton;
     
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+    
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_hello_moon, parent);
         mPlayButton = (Button) v.findViewById(R.id.hellomoon_playButton);
